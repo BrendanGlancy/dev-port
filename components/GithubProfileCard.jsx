e2df4d5f00eb2d354cdcd12211768a8a817955fa
brendanglancy <bglance68@gmail.com>
2021-12-15 02:54:42 -0500

    emailbox

--- components/GithubProfileCard.jsx
+++ components/GithubProfileCard.jsx
@@ -1,13 +1,84 @@
-import React from "react";
+import React, {useState} from "react";
 
-import { Card, Col, Row, Container } from "reactstrap";
-
+import { Card, InputGroup, InputGroupAddon, InputGroupText, Input, Button, CardBody, FormGroup, Col, Row, Container } from "reactstrap";
 import SocialLinks from "../components/SocialLinks";
 import Image from "next/image";
+import classnames from "classnames";
+
 
 const GithubProfileCard = ({ prof }) => {
+	const state = useState("");
 	return (
 		<Card className="section-lg bg-gradient-info shadow-lg border-10">
+          <section className="section section-lg pt-lg-0 section-contact-us">
+            <Container>
+              <Row className="justify-content-center mt--300">
+                <Col lg="8">
+                  <Card className="bg-gradient-secondary shadow">
+                    <CardBody className="p-lg-5">
+                      <h4 className="mb-1">Want to work with me?</h4>
+                      <p className="mt-0">
+                        Your project is very important to me.
+                      </p>
+                      <FormGroup
+                        className={classnames("mt-5", {
+                        })}
+                      >
+                        <InputGroup className="input-group-alternative">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="ni ni-user-run" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Your name"
+                            type="text"
+                          />
+                        </InputGroup>
+                      </FormGroup>
+                      <FormGroup
+                        className={classnames({
+                        })}
+                      >
+                        <InputGroup className="input-group-alternative">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="ni ni-email-83" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Email address"
+                            type="email"
+                          />
+                        </InputGroup>
+                      </FormGroup>
+                      <FormGroup className="mb-4">
+                        <Input
+                          className="form-control-alternative"
+                          cols="80"
+                          name="name"
+                          placeholder="Type a message..."
+                          rows="4"
+                          type="textarea"
+                        />
+                      </FormGroup>
+                      <div>
+                        <Button
+                          block
+                          className="btn-round"
+                          color="default"
+                          size="lg"
+                          type="button"
+                        >
+                          Send Message
+                        </Button>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </section>
 			<Container className="">
 				<div className="p-2">
 					<Row className="">
