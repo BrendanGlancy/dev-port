3a9fc89b65b3e835b54c965edfea02998e628dab
brendanglancy <bglance68@gmail.com>
2024-09-26 21:02:51 -0400

    website

--- node_modules/next/dist/server/utils.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="zen-observable" />
-import Observable from 'next/dist/compiled/zen-observable';
-export declare function isBlockedPage(pathname: string): boolean;
-export declare function cleanAmpPath(pathname: string): string;
-export declare type RenderResult = Observable<string>;
-export declare function mergeResults(results: Array<RenderResult>): RenderResult;
-export declare function resultsToString(results: Array<RenderResult>): Promise<string>;
