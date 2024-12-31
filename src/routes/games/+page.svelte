1361e2d32ecf56555761ae81128154ae404cfc97
brendanglancy <bglance68@gmail.com>
2024-12-31 02:22:52 -0500

    ematchi

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,55 +1,63 @@
 <script>
     import { games } from "./games";
-    import "../../styles.css";
+    import "../../ematchi.css";
 </script>
 
 <svelte:head>
     <title>Cool Math Games</title>
 </svelte:head>
 
-<div class="game-container">
-    {#each games as game}
-        <a href={game.link}>
-            <div class="game">
-                <p>{game.label}</p>
-            </div>
-        </a>
-    {/each}
+<div class="arcade">
+    <header>
+        <h1>Arcade</h1>
+        <p>Play Games in your browser</p>
+    </header>
+    <div class="game-container">
+        {#each games as game}
+            <a href={game.link}>
+                <div class="game">
+                    <p>{game.label}</p>
+                </div>
+            </a>
+        {/each}
+    </div>
 </div>
 
 <style>
     .game-container {
         display: flex;
         flex-direction: row;
-        justify-content: center;
-        align-items: center;
         font-size: min(1vmin, 0.5em);
+        justify-content: center;
         gap: 2em;
-        height: 100%;
     }
 
     .game {
         display: grid;
-        grid-template-columns: repeat(var(--size), 1fr);
-        grid-template-rows: repeat(var(--size), 1fr);
-        grid-gap: 1em;
-        width: 80em;
+        width: 40em;
         aspect-ratio: 1;
-        perspective: 100vw;
         border-radius: 2em;
-        background: var(--bg-2);
+        background: var(--bg-1);
     }
 
-    p {
+    .game p {
         display: flex;
         justify-content: center;
         align-items: center;
-        font: grandstander;
         font-size: 4em;
+        font-family: grandstander;
     }
 
     a {
         text-decoration: none;
-        color: #ccc;
+        color: #fff;
+    }
+
+    header {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-family: grandstander;
+        padding: 2em;
     }
 </style>
