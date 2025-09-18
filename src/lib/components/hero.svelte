46a57e2f22e94e1f7348fc4b0c4a47cd0975f6a1
BrendanGlancy <bglance68@gmail.com>
2025-09-18 14:46:13 -0400

    added secret table

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,22 +1,63 @@
 <script>
     let mouseEntered = false;
+
+    let bench = 425;
+    let weight = 210;
+    let heightIn = 72;
+    let credit = 800;
+
+    function handleEnter() {
+        mouseEntered = true;
+        bench += 10;
+        weight += 5;
+        height += 1;
+        credit += 10;
+    }
+
+    function handleLeave() {
+        mouseEntered = false;
+    }
+
+    const fmtHeight = (inches) => {
+        const ft = Math.floor(inches / 12);
+        const inch = inches % 12;
+        return `${ft}'${inch}`;
+    };
 </script>
 
 <main id="hero" class:entered={mouseEntered}>
     <h2>Brendan Glancy</h2>
     <table
         class:entered={mouseEntered}
-        on:mouseenter={() => (mouseEntered = true)}
-        on:mouseleave={() => (mouseEntered = false)}
+        on:mouseenter={handleEnter}
+        on:mouseleave={handleLeave}
     >
         <thead>
-            <tr class:entered={mouseEntered}>
+            <tr>
                 <th>Bench</th>
+            </tr>
+            <tr>
+                <th>Weight</th>
+            </tr>
+            <tr>
+                <th>Height</th>
+            </tr>
+            <tr>
+                <th>Credit Score</th>
             </tr>
         </thead>
         <tbody>
-            <tr class:entered={mouseEntered}>
-                <td>425 Lbs</td>
+            <tr>
+                <td>{bench} Lbs</td>
+            </tr>
+            <tr>
+                <td>{weight} Lbs</td>
+            </tr>
+            <tr>
+                <td>{fmtHeight(heightIn)}</td>
+            </tr>
+            <tr>
+                <td>{credit}</td>
             </tr>
         </tbody>
     </table>
@@ -24,7 +65,7 @@
 
 <style>
     main {
-        background: linear-gradient(27deg, #2b2b2b, black, black);
+        background: black;
         display: flex;
         flex-direction: column;
         justify-content: center;
